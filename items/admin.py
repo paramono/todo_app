3c60774e2ea7a52743cb8d8683af4fc2944696b5
paramono <alex@paramono.com>
2021-06-03 03:02:39 +0300

    Add admin for todo lists/items

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import ListContainer, ListItem
 
-# Register your models here.
+
+class ListItemInline(admin.TabularInline):
+    model = ListItem
+    raw_id_fields = ['parent']
+    extra = 1
+
+
+@admin.register(ListContainer)
+class ListContainerAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    list_editable = ['name']
+    inlines = [ListItemInline]
+
+
+@admin.register(ListItem)
+class ListItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'parent']
+    list_editable = ['name']
+    raw_id_fields = ['parent']
