6516b99172681659f2b49d5e38fbc8fbce6951b5
paramono <alex@paramono.com>
2021-06-03 03:00:43 +0300

    Configure API endpoints for todo lists/items

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from todo_app.users.api.views import UserViewSet
+from items.views import ListContainerViewSet, ListItemViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,6 +10,8 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("containers", ListContainerViewSet, basename=r'containers')
+router.register("items", ListItemViewSet, basename=r'items')
 
 
 app_name = "api"
--- config/settings/base.py
+++ config/settings/base.py
@@ -278,7 +278,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+    'DEFAULT_PAGINATION_CLASS': 'todo_app.pagination.DefaultPagination',
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
